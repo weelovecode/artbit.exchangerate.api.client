0e233d950a32c334cb50f6b7f39d24712c0a20c2
weelovecode <107809945+weelovecode@users.noreply.github.com>
2023-02-28 17:12:33 +0300

    initial commit

---
+++ src/Abstractions/IExchangeRateApiClient.cs
@@ -0,0 +1,10 @@
+﻿namespace artbit.ExchangeRate.API.Client.Abstractions
+{
+    using Implementations;
+
+    public interface IExchangeRateApiClient
+    {
+        Task<ICollection<P2PTradeModel>> BestAsync(int? tradeDirection, string direction, P2PTradeFilterModel body);
+        Task<ICollection<P2PTradeModel>> BestAsync(int? tradeDirection, string direction, P2PTradeFilterModel body, CancellationToken cancellationToken);
+    }
+}
