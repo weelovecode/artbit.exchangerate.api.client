e6a49da82108ec7270f464214a66fb0d7f36c1a4
weelovecode <107809945+weelovecode@users.noreply.github.com>
2023-03-06 23:50:25 +0300

    Update api client contracts: rates, options

--- src/Abstractions/IExchangeRateApiClient.cs
+++ src/Abstractions/IExchangeRateApiClient.cs
@@ -4,7 +4,15 @@
 
     public interface IExchangeRateApiClient
     {
-        Task<ICollection<P2PTradeModel>> BestAsync(int? tradeDirection, string direction, P2PTradeFilterModel body);
-        Task<ICollection<P2PTradeModel>> BestAsync(int? tradeDirection, string direction, P2PTradeFilterModel body, CancellationToken cancellationToken);
+        /// <summary>
+        /// Get lastest rate with filters
+        /// </summary>
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        Task<ICollection<P2PTradeModel>> LatestAsync(TradeDirectionEnum? directionQuery, string directionPath, P2PTradeFilterModel body);
+
+        /// <returns>Success</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        Task<ICollection<ValueTuple_2OfExchangeEnumAndIEnumerable_1OfP2PTradeModel>> BestAsync(TradeDirectionEnum? directionQuery, string directionPath, P2PTradeFilterModel body);
     }
 }
